1651537447293625b8283b25c20ca8eb83ef13b7
mikhawa <michaeljpitz@gmail.com>
2019-06-06 16:51:40 +0200

    hash.php

--- prepa/hash.php
+++ prepa/hash.php
@@ -3,11 +3,22 @@
 $mdp = ["admin","test","xc25d9"];
 
 foreach ($mdp AS $key => $item){
-    $arr[]=password_hash($item,PASSWORD_DEFAULT);
+    $arr[$key]=password_hash($item,PASSWORD_DEFAULT);
     $unique[]=uniqid('key',true);
     echo (password_verify($item,$arr[$key]))? "ok<br>" : "ko<br>";
 }
 
+echo "<pre>";
+var_dump($arr,$unique);
+echo "</pre>";
+
+foreach ($mdp AS $key => $item){
+    $arr[$key]=password_hash($item,PASSWORD_DEFAULT);
+    echo (password_verify($item,$arr[$key]))? "ok<br>" : "ko<br>";
+}
+foreach ($mdp AS $key => $item){
+    echo (password_verify($item,$arr[$key]))? "ok<br>" : "ko<br>";
+}
 echo "<pre>";
 var_dump($arr,$unique);
 echo "</pre>";
