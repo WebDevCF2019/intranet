a23c5c34eb4829a027180ef2e18b0f594c8d94e7
mikhawa <michaeljpitz@gmail.com>
2019-06-07 16:25:17 +0200

    analyse DB

--- prepa/hash.php
+++ prepa/hash.php
@@ -1,4 +1,24 @@
 <?php
+echo "<h3>Codage de admin avec password_hash</h3>";
+echo $a = password_hash("admin",PASSWORD_DEFAULT);
+echo "<br>";
+echo $b = password_hash("admin",PASSWORD_DEFAULT);
+echo "<br>";
+echo $c = password_hash("admin",PASSWORD_DEFAULT);
+echo "<br>";
+echo $d = password_hash("admin",PASSWORD_DEFAULT);
+echo "<br>";
+echo password_verify("admin",$a);
+echo "<br>";
+echo password_verify("admin",$b);
+echo "<br>";
+echo password_verify("admin",$c);
+echo "<br>";
+echo password_verify("admin",$d);
+echo "<br>";
+
+
+
 
 $mdp = ["admin","test","xc25d9"];
 
