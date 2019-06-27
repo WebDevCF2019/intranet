60d6639542ce6a1197e30c7d2a1eec83a9169ac7
Pierre <pierre.sandron@cf2m.be>
2019-06-27 11:14:37 +0200

    exemple Calendrier
    exemple d'utilisation d'un Planning et d'un Calendrier

--- prepa/Pierre/testPlanning.php
+++ prepa/Pierre/testPlanning.php
@@ -1,36 +1,32 @@
 <?php
-    require("Planning.class.php");
-    //require("Conge.class.php");
+    include "header.php";
 
-    //$unPlanning = new Planning();
-    //$unPlanning = new Planning('2019-01-07','2019-07-05');
-    $unPlanning = new Planning(123);
-    //$unConge = new Conge(123);
+    require("Planning.class.php");
+    require("Calendrier.class.php");
+
+    $unPlanning = new Planning(123);    // on suppose que l'ID session vaut 123
+    $unCalendrier = new Calendrier($unPlanning);
 
     echo "<h1>Planning</h1>";
 
-    //echo "<h2>Tous les jours ouvrables du planning (entre le ".$unPlanning->getPremiereDate()." et le ".$unPlanning->getDerniereDate().") :</h2>";
-    //$unPlanning->afficheToutesLesDates();
-    //$unPlanning->afficheTousLesJoursOuvrés();
+    //$unPlanning->afficheListeComplete();
+/*
+    $unPlanning->afficheListeEntreDeuxDates('2019-01-01', '2019-01-31');
+    echo "<hr>";
+*/
 
-    //echo "<h2>Jours ouvrables de avril 2019 :</h2>";
-    //$unPlanning->afficheDatesPourUnMois(6,2019);
-    //$unPlanning->afficheJoursOuvrésPourUnMois(4,2019);
+/*    $unMois = $unPlanning->extraireCalendrier(6,2019);
+    $unPlanning->extraireCalendrier(1,2019);
+    $unPlanning->extraireCalendrier(12,2019);*/
 
-    //echo "<h2>Jours ouvrables entre 2 dates (par exemple, entre le 1er juin et le 15 juin 2019) :</h2>";
-    //$unPlanning->afficheJoursOuvrésEntreDeuxDates('2019-06-01','2019-06-15');
+    $unCalendrier->afficheMois(5, 2019, TRUE);    echo "<hr>";
+    $unCalendrier->afficheMois(11, 2018, FALSE);  echo "<hr>";
+    $unCalendrier->afficheMois(5, 2020, TRUE);    echo "<hr>";
+    $unCalendrier->afficheMois(5, 2022, FALSE);   echo "<hr>";
+    $unCalendrier->afficheMois(11, 2016, FALSE);   echo "<hr>";
 
-    //echo "<h2>Cours de la semaine :</h2>";
-    //$cetteSemaine = $unPlanning->listeCoursSemaine('2019-06-03', '09:00:00', '13:00:00', 'PT3H30M');
-    //$unPlanning->afficheListeCours($cetteSemaine);
+    $unCalendrier->afficheTousLesMois(TRUE);
 
-    //echo "<h2>Cours du mois (exemple : janvier 2019) :</h2>";
-    //$unPlanning->listeCoursMois(6,2019);
 
-    //echo "<h2>Jours Fériés (exemple : 2019) :</h2>";
-    //print_r($unPlanning->calculJoursFeries(2019));
-
-    //echo "<h2>Congés Extra-légaux CF2M (exemple : 2019) :</h2>";
-    //print_r($unPlanning->calculExtraLegalCF2M(2019));
-
+    include_once "footer.php";
 ?>
