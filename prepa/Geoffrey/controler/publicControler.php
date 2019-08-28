7eca233d8a99c730bc4dd6cafbe3471160d18267
devildevweb <devil.devweb@gmail.com>
2019-08-28 13:44:24 +0200

    maj

---
+++ prepa/Geoffrey/controler/publicControler.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * public
+ */
+
+ $menu = $ledroitM->creerMenu();
+
+ /**
+  * 
+  */
+
+  if(isset($_GET['idledroit']) &&ctype_digit($_GET['idledroit'])){
+
+//var_dump($idthefiliere);
+
+    $idledroit = (int) $_GET['idledroit'];
+
+    $detailLedroit = $ledroitM->selectionnerLedroitParId($idlafiliere);
+
+    echo $twig->render("accueilLedroit.html.twig",["lemenu"=>
+    $menu, "detailLedroit"=>$detailLedroit]);
+
+
+  }else {
+
+    $ledroit= $ledroitM->selectionnerLedroitIndexPublic();
+
+    echo $twig->render("accueilLedroit.html.twig", ["lemenu"=>$menu,"detailLedroit"=>$ledroit]);
+  }
