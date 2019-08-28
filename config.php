f7447ddc61b02774cb8a652d4558748b47207bcd
Michael J. Pitz <michaeljpitz@gmail.com>
2019-08-28 15:43:24 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define("DB_HOST","localhost");
 define("DB_NAME","intranetV3");
 define("DB_LOGIN","root");
 define("DB_PWD","");
-define("DB_PORT","3308");
+define("DB_PORT","3306");
 define("DB_CHARSET","utf8");
 
 // default development mode, change to true for product mode
