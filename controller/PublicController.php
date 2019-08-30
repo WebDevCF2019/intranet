2c1bcbbaf63bab9d24282ae6b8826d6bed20d409
mikhawa <michaeljpitz@gmail.com>
2019-08-30 14:50:10 +0200

    correction

--- controller/PublicController.php
+++ controller/PublicController.php
@@ -1,12 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 // page d'accueil
 if(!isset($_GET['connect'])){
     // lien vers la page d'accueil
     echo $twig->render("public/homepage.html.twig");
 }
-=======
-echo "Hello World"; //
-
->>>>>>> 70eb7b3de39571424af534e5a241176f02ead4f0
